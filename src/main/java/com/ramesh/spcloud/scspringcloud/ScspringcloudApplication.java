eff5c11c1a204c359f074949d2732a33e451f62f
Ramesh <rameshkp2@gmail.com>
2021-01-16 01:00:40 +0900

    Inital Commit for Docker cloud.

---
+++ src/main/java/com/ramesh/spcloud/scspringcloud/ScspringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.ramesh.spcloud.scspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScspringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScspringcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramesh/spcloud/scspringcloud/ScspringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramesh.spcloud.scspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScspringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
